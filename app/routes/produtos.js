c8dace9b0dc9a72bd00063ff04cb925abfa16797
Moises Olimpio <molimpio@hotmail.com.br>
2016-07-07 20:49:56 -0300

    add livro no bd

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,17 +1,31 @@
 module.exports = function (app) {
-    app.get('/produtos', function (req, res) {
+
+    var listaProdutos = function (req, res) {
 
         var connection = app.infra.connectionFactory();
-        var produtosBanco = new app.infra.ProdutosDAO(connection);
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
 
-        produtosBanco.lista(function (erros, resultados) {
-            res.render('produtos/lista',{lista: resultados});
+        produtosDAO.lista(function (erros, resultados) {
+            res.render('produtos/lista', {lista: resultados});
         });
         connection.end();
-    });
+    };
+
+    app.get('/produtos', listaProdutos);
 
     app.get('/produtos/form', function (req, res) {
         res.render('produtos/form');
+    });
+
+    app.post('/produtos/salva', function (req, res) {
+
+        var produto = req.body;
+
+        var connection = app.infra.connectionFactory();
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
+        produtosDAO.salva(produto, function (erros, resultados) {
+           res.redirect('/produtos');
+        });
     });
 };
 
